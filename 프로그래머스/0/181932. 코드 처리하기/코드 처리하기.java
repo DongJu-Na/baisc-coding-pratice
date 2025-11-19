cdd612bade95594bd945445e2ddf77f8bbb5e77e
DongJu-Na <79893048+DongJu-Na@users.noreply.github.com>
2025-11-19 09:57:16 +0900

    [level 0] Title: 코드 처리하기, Time: 9.81 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+          int mode = 0;
+StringBuilder ret = new StringBuilder();
+        
+          for (int i = 0; i < code.length(); i++) {
+            char ch = code.charAt(i);
+
+            if (ch == '1') {
+                // mode 토글
+                mode = 1 - mode;
+            } else {
+                if (mode == 0 && i % 2 == 0) {
+                    ret.append(ch);
+                } else if (mode == 1 && i % 2 == 1) {
+                    ret.append(ch);
+                }
+            }
+        }
+        
+        answer = ret.length() == 0 ? "EMPTY" : ret.toString();
+        
+        return answer;
+    }
+}
